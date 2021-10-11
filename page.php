db9dbb4a230566718d53ef2e56615b8471c3e182
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:16:30 +0800

    Add files via upload

--- page.php
+++ page.php
@@ -1,1 +1,18 @@
-
+<?php
+   /*
+   categories =>[Manage | Edit | Update | Add | Insert | Delete ]
+   */
+   
+$do = isset($_GET['do']) ? $_GET['do']:'Manage';
+// if the page is main page 
+if ($do == 'Manage') {
+    echo "WELCOME IN MANAGE PAGE";
+    echo "<br>";
+    echo '<a href="?do=Add">Add a new category + </a>';
+}elseif ($do == 'Add') {
+    echo "WELCOME IN ADD APGE";
+}elseif ($do == 'Insert') {
+    echo "WELCOME IN INSERT APGE";
+}else {
+    echo "THERE IS NO PAGE WITH THIS NAME";
+}
