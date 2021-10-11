37009cb511b2a924d6059a863437bda21b65bd06
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:02:22 +0800

    Add files via upload

---
+++ admin/templates/header.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?php getTitle() ?></title>
+
+
+    <link rel="stylesheet" href="layout/css/bootstrap.min.css">
+    <link rel="stylesheet" href="layout/css/font-awesome.min.css">
+	<link rel="stylesheet" href="layout/css/backend.css">
+	
+</head>
+<body>
+	
