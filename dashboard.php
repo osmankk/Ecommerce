c6adb596551e9c69028a69fe105c82993cafd2ac
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:11:45 +0800

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,1 +1,20 @@
 
+<?php
+session_start();
+                          
+
+if (isset($_SESSION['Username'])) {
+    $pageTitle = 'Dashboard';
+    include 'init.php';
+   
+   
+    include $tpl . "footer.php";
+}else {
+    header('Location:index.php');
+
+}
+     
+?>
+
+
+
