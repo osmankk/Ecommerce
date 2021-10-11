9b4d1baf4774034f2f581c9b34b1d13322145547
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:08:59 +0800

    Add files via upload

--- admin/css/page.php
+++ admin/css/page.php
@@ -1,1 +1,18 @@
+<?php 
+/*
+*/
+$do = isset($_GET['do']) ? $_GET['do'] : 'Manage';
 
+if ($do == 'Manage'){
+    echo 'welcome you are in Mange category page';
+    echo '<a href="?do=Inswerr"> Add new category +</a>';
+
+}elseif ($do == 'Add'){
+    echo 'welcome to ADD apge ';
+}elseif ($do == 'Insert'){
+    echo 'welcome to Insert apge ';
+}else {
+    
+        echo 'error there is noo page with this name ';
+    
+}
