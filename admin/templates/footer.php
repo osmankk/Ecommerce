3f6fd937046193515c013e9969c3afdd397106a6
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:01:38 +0800

    Add files via upload

--- admin/templates/footer.php
+++ admin/templates/footer.php
@@ -1,1 +1,8 @@
-
+		<div class="footer">
+			
+		</div>
+		<script src="layout/js/jquery-1.12.1.min.js"></script>
+		<script src="layout/js/bootstrap.min.js"></script>
+		<script src="layout/js/backend.js"></script>
+</body>
+</html>
