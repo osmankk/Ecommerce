64b264250749ad8eaa10b801b5b5725ae9be04b7
osman kaheel <92308763+osmankk@users.noreply.github.com>
2021-10-11 23:06:37 +0800

    Add files via upload

--- admin/js/backend.js
+++ admin/js/backend.js
@@ -1,1 +1,28 @@
+$(function () {
+	'use strict';
+	$('[placeholder]').focus(function () {
+	$(this).attr('data-text',$(this).attr('placeholder'));
+	$(this).attr('placeholder','');
+	}).blur(function () {
+	$(this).attr('placeholder',$(this).attr('data-text'));
+	});
+	
+});
+// add asterisk on required field
+$('input').each(function () {
+	if ($(this).attr('required')==='required'){
+		$(this).after("<span class='asterisk'> * </span>");
+	}
+	
+});
+//show pass 
+$('.show-pass').hover(function () {
+ $('.password').attr('type','text')	
+},function () {
+	$('.password').attr('type','password')	
+});
+//confirmation delete
+$('.comfirm').click(function () {
+	return confirm('ARE YOU SURE TO DELETE THIS MEMBER ?')
+});
 
