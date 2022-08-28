8d93c8216b99f1ee0c710fb63d9979b078399915
osman kaheel <92308763+osmankk@users.noreply.github.com>
2022-08-29 02:19:27 +0800

    Update English.php

--- admin/includes/languages/English.php
+++ admin/includes/languages/English.php
@@ -1,1 +1,16 @@
 
+<?php
+      function lang($phrase){
+       static $lang = array(
+            'HOME_ADMIN' => 'HOME',
+       	'osman' =>'OSMAN',
+       	'Categories' => 'CATEGORIES',
+            'items'      => 'ITEMS',
+            'members'    => 'MEMBERS',
+            'statistics' => 'STATISTICS',
+            'logs'       => 'LOGS',
+            'comments'   => 'COMMENTS'
+       );
+       return $lang[$phrase];
+      }
+  
